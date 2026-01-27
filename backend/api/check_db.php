78e4b732b47a30654b75199b1ba8dc56a576f860
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2026-01-28 02:56:47 +0600

    Add check_db.php

---
+++ backend/api/check_db.php
@@ -0,0 +1,12 @@
+<?php
+require "../config/db.php";
+
+$result = $conn->query("SHOW COLUMNS FROM posts");
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row['Field'] . " - " . $row['Type'] . "\n";
+    }
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
